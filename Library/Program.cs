43394d5093e06eb7062c5f472f9463c1ab667bd6
Oisin Henry <Henry-O1@ulster.ac.uk>
2024-10-08 10:11:28 +0100

    Initial commit

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+    int NoOfPages;
+
+
+
+void DisplayInfo()
+    {
+        // Output the book information to the console
+        Console.WriteLine("Book information");
+        Console.WriteLine("--------------------");
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"Book pages: {NoOfPages}");
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        // Create a new instance of the book class
+        Book book = new Book();
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+        book.NoOfPages = 200;
+
+        Book book2 = new Book();
+        book2.Title = "C# Methods and classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = "5566778";
+        book2.NoOfPages = 50;
+
+        // ooutput book information to the console
+        book.DisplayInfo();
+        book2.DisplayInfo();
+    }
+
+}
+
